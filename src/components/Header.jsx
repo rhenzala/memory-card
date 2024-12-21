e018b08728f61c829b1a0a96cd2c10aca5a3ff83
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-21 15:13:06 +0800

    Add layout and colors to logo and scoreboard
    Also added an instruction under the logo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Score({
     bestScore
 }) {
     return (
-        <div>
+        <div className="bg-white border-black border-2 border-solid rounded-md p-4">
             <p>Score: {currentScore}</p>
             <p>Best Score: {bestScore}</p>
         </div>
@@ -16,9 +16,10 @@ export default function Header({
     bestScore
 }) {
     return (
-        <header className="flex justify-between gap-3 p-4">
-            <div className="logo">
-                <h1>PokeMind</h1>
+        <header className="flex items-center justify-between gap-4 p-4 bg-red border-b-black border-b-solid border-b-4 custom-sm:flex-col">
+            <div className="logo flex flex-col custom-sm:items-center gap-3">
+                <h1>PokeMemory</h1>
+                <p className="text-sm text-white">Get points by clicking on any image but don't click on any more than once!</p>
             </div>
             <Score
             currentScore={currentScore}
