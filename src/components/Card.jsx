fac94ee9efd2935d907627f23712992119275c7a
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-21 15:12:01 +0800

    Add layout and colors to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,9 +17,17 @@ function CreateCard({pokemon, handleCardClick}) {
     }, []);
     
     return (
-        <div className="card flex flex-col gap-2" onClick={() => handleCardClick(pokemon)}>
-            <img src={imgUrl} alt={pokemon.name}/>
-            <p>{pokemon.name}</p>
+        <div 
+        className="card flex flex-col gap-2 items-center rounded-xl border-blue border-4 border-solid hover:cursor-pointer hover:-translate-y-2" 
+        onClick={() => handleCardClick(pokemon)}
+        >
+            <img
+            className="w-40 h-40 " 
+            src={imgUrl} 
+            alt={pokemon.name}/>
+            <div className="bg-blue w-full py-2 text-center">
+                <p className="text-white capitalize text-lg">{pokemon.name}</p>
+            </div>
         </div>
     )
 }
@@ -27,9 +35,9 @@ function CreateCard({pokemon, handleCardClick}) {
 export default function Card({isStarted, onStart, pokemons, handleCardClick}) {
     
     return (
-        <div>
+        <div className="p-6 grid">
             {isStarted ?
-            <div className="grid gap-4 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
+            <div className="grid gap-4 grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))]">
                 {pokemons.map(pokemon => 
                 <CreateCard
                 key={pokemon.id} 
@@ -39,8 +47,9 @@ export default function Card({isStarted, onStart, pokemons, handleCardClick}) {
                 )} 
             </div>
             : <button
-            className="capitalize p-3"
+            className="capitalize p-3 bg-yellow border-blue border-2 border-solid text-blue text-lg justify-self-center"
             onClick={onStart}
+            aria-label="Start game"
             >
                 start game
             </button>
